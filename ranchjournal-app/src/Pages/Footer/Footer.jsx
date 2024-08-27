bff9fe7fbf27b38fc5864f271039a0105cf3f5c8
Jamshid <121611179+Jamshid525@users.noreply.github.com>
2024-08-27 22:50:38 +0500

    footer responsive

--- ranchjournal-app/src/Pages/Footer/Footer.jsx
+++ ranchjournal-app/src/Pages/Footer/Footer.jsx
@@ -6,23 +6,21 @@ export default function Footer() {
     return (
 
 
-        <footer className="Fotter">
-            <div className="FotterCont">
-                <div>
-                    <h4 >Ranch Journal</h4>
-                    <p>Muassis: Urganch Ranch texnologiya universiteti "Fan va
-                        <br />ta'limdagi zamonaviy tadqiqotlar." jurnalining muassisi
-                        <br /> hisoblanadi. https://www.utu-ranch.uz/</p>
-                </div>
-                <div>
-                    <h5>Manzil</h5>
-                    <p>Tahririyat joylashgan manzil – O'zbekiston Respublikasi,<br /> Urganch shahri, Xonqa ko'chasi 26.</p>
-                </div>
-                <div>
-                    <h5>Bog'lanish</h5>
-                    <p>Phone: +998 90 3239923,<br /> Phone: +998 97 7001248</p>
-                    <p>Email: ranchjournal@utu-ranch.uz</p>
-                </div>
+        <footer className="FotterContainer">
+            <div>
+                <h4 >Ranch Journal</h4>
+                <p>Muassis: Urganch Ranch texnologiya universiteti "Fan va
+                    <br />ta'limdagi zamonaviy tadqiqotlar." jurnalining muassisi
+                    <br /> hisoblanadi. https://www.utu-ranch.uz/</p>
+            </div>
+            <div>
+                <h5>Manzil</h5>
+                <p>Tahririyat joylashgan manzil - O'zbekiston Respublikasi,<br /> Urganch shahri, Xonqa ko'chasi 26.</p>
+            </div>
+            <div>
+                <h5>Bog'lanish</h5>
+                <p>Phone: +998 90 3239923,<br /> Phone: +998 97 7001248</p>
+                <p>Email: ranchjournal@utu-ranch.uz</p>
             </div>
         </footer>
     )
