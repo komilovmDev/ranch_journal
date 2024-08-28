21da4c598336a980289328b787799b2e0033b7e8
Jamshid <121611179+Jamshid525@users.noreply.github.com>
2024-08-28 13:45:41 +0500

    dshjfd

--- ranchjournal-app/src/Pages/ArxivMinth/ArxivMonth.jsx
+++ ranchjournal-app/src/Pages/ArxivMinth/ArxivMonth.jsx
@@ -3,22 +3,23 @@ import { useContext, useEffect, useState } from "react";
 import { FaArchive } from "react-icons/fa";
 import { Link, useParams } from "react-router-dom";
 import AppContext from "../../context/AppContext";
+import './arxivMonth.css'
 
 export default function ArxivMonth() {
 
-    const [data, setData] = useState([])
-    const id = useParams()
+    // const [data, setData] = useState([])
+    // const id = useParams()
 
-    const getArxivMonth = async () => {
-        const response = await axios.get(`https://api.ranchjournal.uz/arxiv/arxiv-cats/${id.id}/`);
-        setData(response.data.results)
-    }
+    // const getArxivMonth = async () => {
+    //     const response = await axios.get(`https://api.ranchjournal.uz/arxiv/arxiv-cats/${id.id}/`);
+    //     setData(response.data.results)
+    // }
 
-    useEffect(() => {
-        getArxivMonth()
-    }, [])
+    // useEffect(() => {
+    //     getArxivMonth()
+    // }, [])
 
-    const {leng , setLeng} = useContext(AppContext)
+    const { leng, setLeng } = useContext(AppContext)
 
     return (
         <div className="Month">
@@ -27,19 +28,29 @@ export default function ArxivMonth() {
                     <h1>Arxiv</h1>
                 </div>
                 <div className="ArxivContainersBox">
-                    {data.map(item => (
-                        <Link key={item.id} to={`/arxivview/${item.id}`}>
-                            <div className="ArxivContainers">
-                                <div className="ContainersIcons">
-                                    <FaArchive />
-                                </div>
-                                <div className="ContainersText">
-                                    {leng == 'uz' ? item.name : leng == 'ru' ? item.name_ru : item.name_en}
-                                </div>
-                            </div>
-                        </Link>
-                    ))}
+                    <aside className="ArxiveBox">
+                        <ul>
+                            <li>
+                                <a href="">2024</a>
+                            </li>
+                            <li>
+                                <a href="">2023</a>
+                            </li>
+                            <li>
+                                <a href="">2022</a>
+                            </li>
+                        </ul>
+                    </aside>
+                    <div className="ArxiveBoxs">
+                        <div className="arxiveBoxs1">
+                            <p>a</p>
+                        </div>
+                        <div className="arxiveBoxs1">
+                            <p>a</p>
+                        </div>
+                    </div>
                 </div>
+
             </section>
         </div>
     )
