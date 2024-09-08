b0fe6ade175d4d7691c4d5c7276d6365dcfc68a1
Jamshid <121611179+Jamshid525@users.noreply.github.com>
2024-09-08 16:49:19 +0500

    dahsjda

--- ranchjournal-app/src/Pages/Taxririyat/Taxririyat.jsx
+++ ranchjournal-app/src/Pages/Taxririyat/Taxririyat.jsx
@@ -77,7 +77,7 @@ export default function Taxririyat() {
                 <div className="JournalButton">
                     {data && data.results && data.results.length > 0 ? (
                         data.results.map(item => (
-                            <Link to={`/journal/${item.id}`} key={item.id}>
+                            <Link to={`/jurnalwiev/${item.id}`} key={item.id}>
                                 {item.name}
                             </Link>
                         ))
