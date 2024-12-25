39b708e625f63d4267db8f13cc367b185b7240d1
Muhammad Komilov <120216920+komilovmDev@users.noreply.github.com>
2024-12-25 16:55:47 +0500

    aa

--- ranchjournal-app/src/Pages/jurnalTalablari/JurnalTalablari.jsx
+++ ranchjournal-app/src/Pages/jurnalTalablari/JurnalTalablari.jsx
@@ -1,13 +1,13 @@
 import archive from '../../assets/j3.jpg';
-import archive2 from '../../assets/j4.jpg'; 
+import archive2 from '../../assets/j4.jpg';
 import './jurnal.css';
 
 export default function JurnalTalablari() {
     return (
         <div className="ArchiveMain">
             <section className='Archivview'>
-                <img src={archive} alt="Another Journal Requirement" />
                 <img src={archive2} alt="Journal Requirements" />
+                <img src={archive} alt="Another Journal Requirement" />
             </section>
         </div>
     );
